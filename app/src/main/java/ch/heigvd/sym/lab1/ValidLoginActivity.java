985bf2f28b98021ae949049e0f4c7e94e3d62ae9
Edward Ransome <edward.ransome@heig-vd.ch>
2017-10-12 17:37:22 +0200

    Added Log in each onSomething() function to trace calls

--- app/src/main/java/ch/heigvd/sym/lab1/ValidLoginActivity.java
+++ app/src/main/java/ch/heigvd/sym/lab1/ValidLoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,9 +20,11 @@ public class ValidLoginActivity extends AppCompatActivity {
     private ImageView photo = null;
     private TextView mail = null;
     private TextView imei = null;
+    private static final String LOG_TAG = "Exercice 8: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "OnCreate() called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_valid_login);
 
@@ -51,5 +54,40 @@ public class ValidLoginActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onPause(){
+        Log.d(LOG_TAG, "OnPause() called");
+        super.onPause();
+    }
+
+    @Override
+    public void onResume(){
+        Log.d(LOG_TAG, "OnResume() called");
+        super.onResume();
+    }
+
+    @Override
+    public void onStop(){
+        Log.d(LOG_TAG, "OnStop() called");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy(){
+        Log.d(LOG_TAG, "OnDestroy() called");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onStart(){
+        Log.d(LOG_TAG, "OnStart() called");
+        super.onStart();
+    }
+
+    @Override
+    public void onRestart(){
+        Log.d(LOG_TAG, "OnRestart() called");
+        super.onRestart();
+    }
 
 }
